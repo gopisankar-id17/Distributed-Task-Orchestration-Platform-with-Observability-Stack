26bec852a724bc73831079bf7433dff574d7967b
Gopi sankar <gopins172@gmail.com>
2026-03-17 01:09:55 +0530

    Update response message in app.js to "Vanakam da mapla"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello DevOps World!");
+  res.send("Vanakam da mapla");
 });
 
 app.listen(3000, () => {
