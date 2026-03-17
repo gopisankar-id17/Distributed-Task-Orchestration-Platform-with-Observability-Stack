d2dc9917533c467460576050c8226db53d1dec4f
Gopi sankar <gopins172@gmail.com>
2026-03-17 20:49:22 +0530

    Add index.html for Todo app UI and restructure app.js

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("hi how are you tdy...just checking...im eating");
+app.use(express.json());
+app.use(express.static('public'));
+
+let todos = [];
+
+// Get all todos
+app.get('/todos', (req, res) => {
+    res.json(todos);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-}); 
+// Add todo
+app.post('/todos', (req, res) => {
+    const todo = {
+        id: Date.now(),
+        text: req.body.text,
+        completed: false
+    };
+    todos.push(todo);
+    res.json(todo);
+});
+
+// Delete todo
+app.delete('/todos/:id', (req, res) => {
+    todos = todos.filter(t => t.id !== parseInt(req.params.id));
+    res.json({ success: true });
+});
+
+// Toggle complete
+app.patch('/todos/:id', (req, res) => {
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (todo) todo.completed = !todo.completed;
+    res.json(todo);
+});
+
+app.listen(3000, () => console.log('Todo app running on port 3000'));
